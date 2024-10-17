a4610534797ace1011b1731cf8bed0998f6c9dc5
kayoMichael <michaelxkayo@hotmail.com>
2024-10-17 14:49:01 -0400

    Make Ranking Fetch more dynamic

--- premier_league/utils/methods.py
+++ premier_league/utils/methods.py
@@ -14,6 +14,7 @@ def clean_xml_text(text: Union[str, list]) -> str:
 def remove_qualification_and_relegation(data):
     result = []
     skip_next = False
+    counter = 0
 
     for i, item in enumerate(data):
         if skip_next and not item.isdigit():
@@ -24,7 +25,13 @@ def remove_qualification_and_relegation(data):
         if isinstance(item, str) and (item.startswith("Qualification") or item.startswith("Relegation")):
             skip_next = True
             continue
+        if item == "(C)" or item == "(R)":
+            continue
+
+        if isinstance(item, str) and len(item) == 1 and not item.isdigit() and counter > 10:
+            continue
 
         result.append(item)
+        counter += 1
 
     return result
