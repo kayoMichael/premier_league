333085126a4d9376e9e797dc6b317b674afbc4e7
kayoMichael <michaelxkayo@hotmail.com>
2024-12-24 08:01:31 -0800

    Add github workflow to release to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version="0.1",
     author="Michael Li",
     description="Premier League data Scrapping package",
-    packages=find_packages(),
+    packages=['premier_league', 'premier_league.players', 'premier_league.ranking',
+              'premier_league.transfers', 'premier_league.utils'],
     install_requires=[
         'reportlab==4.0.4',
         'requests>=2.28.1',
